e218dd568580f8a0b77a5bb4f858daa2d8af862d
Ritik Singh <ritik.singh@spit.ac.in>
2020-01-06 21:38:47 +0530

    second commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CRISPY_TEMPLATES_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+	image= models.ImageField(default='default.png',upload_to='profile_pics')
+
+	bio= models.CharField(max_length=200,default="No bio")
+
+	def __str__(self):
+
+		return f'{self.user.username} Profile'
+
+class Follow(models.Model):
+      following = models.ForeignKey(User, related_name="who_follows",on_delete=models.CASCADE)
+      follower = models.ForeignKey(User, related_name="who_is_followed",on_delete=models.CASCADE)
+      follow_time = models.DateTimeField(auto_now=True)
+
+      def __unicode__(self):
+          return str(self.follow_time)
+
+      def __str__(self):
+      
+          return "{} follows {}".format(self.following,self.follower)        
+
